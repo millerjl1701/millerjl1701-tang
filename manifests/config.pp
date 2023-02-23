6d0226de04b67f986d1c2dc713ea7f8d91f39991
Niklaus Hofer <niklaus.hofer@stepping-stone.ch>
2023-02-23 17:14:09 +0100

    Add configuration option for the network port Tang is supposed to listen on.

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,6 +2,17 @@
 #
 # This class is called from tang for service config.
 #
-class tang::config {
+class tang::config (
+  Optional[Integer[1,65536]] $listen_port = undef,
+){
   assert_private('tang::config is a private class')
+  include tang
+
+  if $listen_port {
+    systemd::dropin_file { "Make Tang listen on poort ${listen_port}":
+      filename => 'override.conf',
+      unit     => $tang::service_name,
+      content  => "[Socket]\nListenStream=${listen_port}",
+    }
+  }
 }
