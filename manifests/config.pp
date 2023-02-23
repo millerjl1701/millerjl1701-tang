02501d4f968cb70f40d222a54cead82f969ece66
Niklaus Hofer <niklaus.hofer@stepping-stone.ch>
2023-02-23 17:46:25 +0100

    Handle SELinux when listening on different port.

--- manifests/config.pp
+++ manifests/config.pp
@@ -14,5 +14,17 @@ class tang::config (
       unit     => $tang::service_name,
       content  => "[Socket]\nListenStream=\nListenStream=${listen_port}",
     }
+
+    selinux::port { "Allow Tang to listen on port ${listen_port}":
+      seltype  => 'tangd_port_t',
+      protocol => 'tcp',
+      port     => $listen_port,
+    }
+  } else {
+    systemd::dropin_file { "Make Tang listen on poort ${listen_port}":
+      ensure   => absent,
+      filename => 'override.conf',
+      unit     => $tang::service_name,
+    }
   }
 }
